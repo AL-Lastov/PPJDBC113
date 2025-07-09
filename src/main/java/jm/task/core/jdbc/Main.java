3947adcaf76c56bc78b597c7e0fc4555fdd01942
AL <alexander.lastovs@gmail.com>
2025-07-09 12:27:35 +0300

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("aaa", null, (byte) 12);
+        userService.saveUser("rrr", "ggg", (byte) 13);
+        userService.saveUser(null, "nnn", (byte) 14);
+
+        List<User> users = userService.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+
+        }
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
